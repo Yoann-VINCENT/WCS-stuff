88dbc26cf1b58e4e250cc574c83e8d56cd3c7f24
Yoann PARDO-VINCENT <65440660+Yoann-VINCENT@users.noreply.github.com>
2020-09-28 11:38:46 +0200

    Create IndyMovies.php

---
+++ IndyMovies.php
@@ -0,0 +1,9 @@
+<?php
+    $movies = [
+        "Les Aventuriers de l'arche perdue" => '1981',
+        "Indiana Jones et le Temple maudit" =>'1984',
+        "Indiana Jones et la Dernière Croisade" =>'1989'
+    ];
+    foreach ($movies as $movieTittle => $releaseDate){
+        echo "$releaseDate - $movieTittle <br />";
+    }
